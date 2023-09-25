9972d7d7d066341c397a45c7e64cf49912687b9b
Hsabonchi <developersabon@gmail.com>
2023-09-25 10:20:53 -0500

    create an empty spring boot application

---
+++ src/main/java/com/sabonchi/eventlistner/EventListnerApplication.java
@@ -0,0 +1,13 @@
+package com.sabonchi.eventlistner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventListnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventListnerApplication.class, args);
+	}
+
+}
